271c5a945d1791b4014136b2d23be790e548e4b8
supwjm <supwjm@163.com>
2020-07-12 23:18:23 +0800

    feat : 1、初始化

---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+function TestPage() {
+    return <div>
+        <Link href="/">
+            <a>index</a>
+          </Link>
+    </div>
+}
+
+export default TestPage
